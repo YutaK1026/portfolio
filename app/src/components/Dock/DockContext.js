fe3aca344f2a8680de9aae5217836a5909e56eb1
YutaK1026 <inheritans1026@gmail.com>
2023-04-13 10:33:32 +0900

    fix App

--- app/src/components/Dock/DockContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react'
-import { SpringValue } from '@react-spring/web'
-
-
-export const DockContext = createContext({ width: 0, hovered: false, setIsZooming: () => {} })
-
-export const useDock = () => {
-  return useContext(DockContext)
-}
