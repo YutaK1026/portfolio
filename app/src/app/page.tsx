834e3e6de0c2a97a34e50d3d133ac31cded67966
kawachan <inheritans1026@gmail.com>
2024-01-08 18:25:40 +0900

    deploy

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import { redirect } from 'next/navigation'
-export const runtime = 'edge';
+
 export default function Home() {
   redirect("/about")
 }
