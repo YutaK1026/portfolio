441a3dd9eb5fc9c62cb57baddb69af1bc6b8fd82
kawachan <inheritans1026@gmail.com>
2024-01-10 11:11:57 +0900

    change-Metadata変更

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,7 +3,7 @@ import RootLayout from './AppLayout';
 
 export const metadata: Metadata = {
   title: "kawamura portfolio",
-  description: '河村悠太のポートフォリオサイトです。',
+  description: '河村悠太のポートフォリオサイト',
 }
 
 export default RootLayout;
