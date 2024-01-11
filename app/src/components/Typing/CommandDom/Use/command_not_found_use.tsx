668c3a0178e8600264ddc7dd35e4bf64b6b91f57
kawachan <inheritans1026@gmail.com>
2024-01-11 13:59:03 +0900

    add-use not found

---
+++ app/src/components/Typing/CommandDom/Use/command_not_found_use.tsx
@@ -0,0 +1,8 @@
+export default function CommandNotFoundUse(name: string): React.JSX.Element{
+    const database_name = name
+    return(
+        <div>
+            ERROR 1049 (42000): Unknown database '{database_name}'
+        </div>
+    )
+}
