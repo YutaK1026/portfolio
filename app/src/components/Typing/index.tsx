4c88b6cc3d759fe127a310c2b11f3f2a5f15c253
kawachan <inheritans1026@gmail.com>
2024-01-11 13:58:05 +0900

    add-canvas作成

--- app/src/components/Typing/index.tsx
+++ app/src/components/Typing/index.tsx
@@ -1,19 +1,37 @@
+"use client"
 import MySQLConsoleInput from "./input";
 import { useState } from 'react';
 import {Typing} from "./typing";
+import Command from "./command";
+import { redirect } from 'next/navigation'
 
 export default function TypingPage(){
-    const [domList, setDomList] = useState<React.JSX.Element[]>([
+    const [is_redirect, setIsRedirect] = useState([false,""])
+    
+    const [domList, setDomList] = useState<(React.JSX.Element)[]>([
         <Typing handleFinish={handleFinish}/>
     ]);
 
-    function handleSubmitCommand(){
-        // mysql > の時にコマンドを入力して送信したときの処理
-        setDomList((domList) => [...domList, <MySQLConsoleInput handleSubmitCommand={handleSubmitCommand}/>])
+    function handleSubmitCommand(value: string){
+        const commnad = new Command(value)
+        console.log("入力値:",value)
+        const new_command: React.JSX.Element | string =  commnad.action()
+
+        if(typeof new_command === "string"){
+            // stringの時の処理。clearとaddressを想定
+            setIsRedirect([true, new_command])
+        }else{
+            setDomList((domList) => [...domList, new_command])
+            setDomList((domList) => [...domList, <MySQLConsoleInput handleSubmitCommand={handleSubmitCommand}/>])
+        }
     }
 
     function handleFinish(){
         setDomList((domList) => [...domList, <MySQLConsoleInput handleSubmitCommand={handleSubmitCommand}/>])
+    }
+
+    if(is_redirect[0]){
+        redirect("./"+is_redirect[1])
     }
     
     return(
