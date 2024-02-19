7d3a1d4847541f8aa82182b2cb4879e0169574b1
kawachan <inheritans1026@gmail.com>
2024-02-19 12:33:55 +0900

    スマホ版サイト準備

--- app/src/app/about/page.tsx
+++ app/src/app/about/page.tsx
@@ -1,12 +1,36 @@
 'use client'
 import AboutPage from "@/components/pages/about"
 import Introduce from "@/components/pages/about/introduce"
+import { useEffect, useState } from "react"
 
 export default function About() {
+  const [isPcType, setIsPcType] = useState(false)
+  useEffect(() => {
+    const mobileTypeList = ["iPhone", "iPad", "iPod", "Android"];
+    const machineType = navigator.userAgent;
+    const mobileTypeCheck = mobileTypeList.filter((item) => {
+      return machineType.search(item) != -1
+    })
+    if(mobileTypeCheck.length > 0){
+      setIsPcType(false)
+    }else{
+      setIsPcType(true)
+    }
+    console.log(machineType)
+  })
   return (
     <div>
-      <AboutPage/>
-      <Introduce/>
+      {
+        isPcType
+        ? 
+        <div>aa</div> 
+        :
+        <div>
+          <AboutPage/>
+          <Introduce/>
+        </div>
+      }
+      
     </div>
   )
 }
--- app/src/components/pages/about/index.tsx
+++ app/src/components/pages/about/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-
+import { useEffect } from 'react'
 export default function AboutPage() {
   const src: string = "/props/about/home.webp"
   const imageStyle = {
