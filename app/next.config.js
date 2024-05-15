d003b68bc839a53775972a76403fe281900fa1a7
kawachan <inheritans1026@gmail.com>
2024-05-15 23:09:58 +0900

    change-next.config

--- app/next.config.js
+++ app/next.config.js
@@ -2,7 +2,19 @@
 const nextConfig = {
     "output": 'export',
     images: { unoptimized: true },
-    reactStrictMode: false, 
+    reactStrictMode: false,
+    webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      dns: false,
+      child_process: false,
+      tls: false,
+    };
+
+    return config;
+  },
   }
 
 module.exports = nextConfig
